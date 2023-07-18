addf35931d8dbe14b371ef7119293a643f6306c3
surusuruya <kimsujeong95@naver.com>
2023-07-18 17:32:17 +0900

    login link

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,7 +1,8 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Link } from 'react-router-dom';
 import palette from '../../lib/styles/palette';
 
-const StyledButton = styled.button`
+const buttonStyle = css`
     border: none;
     border-radius: 4px;
     font-size: 1rem;
@@ -15,8 +16,41 @@ const StyledButton = styled.button`
     &:hover {
         background: ${palette.gray[6]};
     }
+
+    ${props =>
+        props.fullWidth &&
+        css`
+            padding-top: 0.75rem;
+            padding-bottom: 0.75rem;
+            width: 100%;
+            font-size: 1.125rem;
+        `
+    }
+
+    ${props => 
+        props.cyan &&
+        css`
+            background: ${palette.cyan[5]};
+            &:hover {
+                background: ${palette.cyan[4]};
+            }
+        `
+    }
 `
 
-const Button = props => <StyledButton {...props} />;
+const StyledButton = styled.button`
+    ${buttonStyle}
+`
 
+const StyledLink = styled(Link)`
+    ${buttonStyle}
+`
+
+const Button = props => {
+    return props.to ? (
+        <StyledLink {...props} cyan={props.cyan ? 1 : 0} />
+    ) : (
+        <StyledButton {...props} />
+    );
+}
 export default Button;
