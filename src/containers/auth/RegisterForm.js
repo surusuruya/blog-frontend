604e49d729b47401363a38f79165975d485d136f
surusuruya <kimsujeong95@naver.com>
2023-07-19 16:38:35 +0900

    p_771 error

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { changeField, initializeForm } from "../../modules/auth";
+import { changeField, initializeForm, register } from "../../modules/auth";
 import AuthForm from "../../components/auth/AuthForm";
 
 const RegisterForm = () => {
     const dispatch = useDispatch();
-    const { form } = useSelector(({ auth }) => ({
-        form: auth.register
+    const { form, auth, authError } = useSelector(({ auth }) => ({
+        form: auth.register,
+        auth: auth.auth,
+        authError: auth.authError
     }))
 
     // input 변경 이벤트 핸들러
@@ -24,13 +26,30 @@ const RegisterForm = () => {
     // form 등록 이벤트 핸들러
     const onSubmit = e => {
         e.preventDefault();
-        // 구현 예정
+        const { username, password, passwordConfirm } = form;
+        if (password !== passwordConfirm) {
+            return;
+        }
+        dispatch(register({ username, password }));
     }
 
     // 컴포넌트가 처음 렌더링될 대 form을 초기화함
     useEffect(() => {
         dispatch(initializeForm('register'));
     }, [dispatch]);
+
+    // 회원가입 성공/실패 처리
+    useEffect(() => {
+        if (authError) {
+            console.log('오류 발생');
+            console.log(authError);
+            return;
+        }
+        if (auth) {
+            console.log('회원가입 성공!');
+            console.log(auth);
+        }
+    }, [auth, authError]);
 
     return (
         <AuthForm 
